cfaaf7dde3511150ae22ea2d419f46fd3438897d
sampojwalam <sampojwalam@gmail.com>
2020-10-03 19:49:59 -0500

    Created Ui, added assets, fonts, etc

---
+++ lib/models/class.dart
@@ -0,0 +1,21 @@
+class Class {
+  final int crn;
+  final String subject;
+  final int courseNumber;
+  final int section;
+  final int creditHours;
+  final String instructor;
+  final int capacity;
+  final int actual;
+
+  Class({
+    this.crn,
+    this.subject,
+    this.courseNumber,
+    this.section,
+    this.creditHours,
+    this.instructor,
+    this.capacity,
+    this.actual,
+  });
+}
---
+++ lib/screens/add_class.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class AddClassScreen extends StatefulWidget {
+  static const routeName = '/add-class';
+
+  @override
+  _AddClassScreenState createState() => _AddClassScreenState();
+}
+
+class _AddClassScreenState extends State<AddClassScreen> {
+  final _crnController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add a New Class"),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              colors: [
+                Color(0xFF400000),
+                Color(0xFF900000),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  gapPadding: 5.0,
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(20.0),
+                  ),
+                ),
+                labelText: "CRN",
+              ),
+              controller: _crnController,
+            ),
+          ),
+          RaisedButton.icon(
+            onPressed: () {
+              print("works");
+            },
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            label: Text(
+              "Add Class",
+              style: TextStyle(color: Colors.white),
+            ),
+            elevation: 0,
+            color: Theme.of(context).primaryColor,
+            //materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          )
+        ],
+      ),
+    );
+  }
+}
